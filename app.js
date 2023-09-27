76b405e1b31a7c68b2d2cd868681a5a9a8d5efae
KhoshfekrMH <khoshfekr2001@protonmail.com>
2023-09-27 21:39:14 +0330

    navbar for header is completed

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/assets"));
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
-	res.send("test");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
